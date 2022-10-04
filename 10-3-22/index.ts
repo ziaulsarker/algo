df5c0a9c58811c462b093019a0fa2731138c5428
ziaul sarker <ziaulsarker@gmail.com>
2022-10-04 01:21:29 -0400

    leet code prob 189 rotate right

--- 10-3-22/index.ts
+++ 10-3-22/index.ts
@@ -1,6 +1,4 @@
 // Rotating an arry K number of times;
-
-
 // given an array you need to rotate its elements K number of times, ex -> [1,2,3,4,5] roate by 2 postions to [3,4,5,1,2];
 
 const reverseAnarray =  <T>(arr: Array<T>, start: number, end: number): Array<T> =>  {
@@ -25,9 +23,31 @@ const rotate = (a: any[], n: number, k: number) => {
 }
 
 const test = async () => {
-  let numbers = [-1, -100, 3, 99];
-  rotate(numbers, numbers.length, 3);
+  let numbers = [ 1,2,3,4,5];
+  rotate(numbers, numbers.length, 2);
   console.log(numbers)
 }
 
 test();
+
+
+
+
+
+//  leet code problem 189 rotate arr to right by K steps
+
+const rotateRight = <T>(a: Array<T>, k: number): Array<T> => {
+  let temp: any = [];
+  if (k <= 0) return a;
+  let lastVal = a.pop();
+  temp = a;
+  temp.unshift(lastVal);
+  return rotateRight(temp, k - 1);
+}
+
+const testRight = async () => {
+  let numbers = [10,20,30,40,50,60,70];
+  rotateRight(numbers, 3);
+}
+
+testRight();

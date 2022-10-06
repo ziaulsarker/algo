2c15709786251aad1f7403b645bb4870f21bab29
ziaul sarker <ziaulsarker@gmail.com>
2022-10-06 00:19:11 -0400

    10-6-22: index arr

---
+++ 10-6-22/index.ts
@@ -0,0 +1,5 @@
+//  INDEX ARR
+
+// given an array fo zise N. containing elements from 0 to N-1. All values from 0 to n-1 are present in the array and if they are not present than a -1 is to take its place arrage values of the array so that value i is stored at arr[i];
+
+
